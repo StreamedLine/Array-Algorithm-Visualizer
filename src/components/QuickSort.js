b3bd40e874fc94703c6666ff83da654641e70613
StreamedLine <Dovidlwtn@gmail.com>
2018-07-08 12:31:11 -0400

    add quickSort. needs debugging

---
+++ src/components/QuickSort.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ArrayTracker from '../state/ArrayTracker';
+import Arr from './Arr';
+
+class QuickSort extends React.Component {
+	render() {
+		const arrayToSort = this.props.currentArray;
+
+		const trackedArrs = () => {
+			let arr = arrayToSort.slice(0);
+			const tracker = new ArrayTracker(arr);
+
+			function swap(left, right) {
+				tracker.swap(left, right)
+				//tracker.updateMap();
+			}
+
+			function partition(left, right) {
+				let pivot = right;
+
+				right -= 1;
+
+				while (true) {
+					while (arr[left] < arr[pivot]) {
+						left += 1
+					}
+
+					while (arr[right] > arr[pivot]) {
+						right -= 1
+					}
+
+					if (left >= right) {
+						break
+					} else {
+						swap(left, right)
+					}
+				}
+
+				swap(left, pivot)
+				tracker.markSorted(left);
+				return left
+			}
+
+			function quickSort(left, right) {		
+				if (right - left <= 0) {
+					return
+				}
+				let pivot = partition(left, right)
+
+				quickSort(0, pivot - 1);
+				quickSort(pivot + 1, right);
+			}
+			quickSort(arr);
+
+			tracker.updateMap();
+			const map = tracker.generateMap();
+
+			const arrs = [];
+			for (let i = 0; i < map.length; i++) {
+			  arrs.push(<Arr arr={map[i]} key={i} />)
+			}
+
+			return arrs
+		}
+
+		return (
+			<div>
+				<h3>Quick Sort</h3>
+				{trackedArrs()}
+			</div>
+		)
+	}
+}
+
+export default QuickSort
+
+
+
--- src/containers/SortAlgorithms.js
+++ src/containers/SortAlgorithms.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import InsertionSort from '../components/InsertionSort';
 import BubbleSort from '../components/BubbleSort';
+import QuickSort from '../components/QuickSort';
 
 class SortAlgorithms extends React.Component {
 	render() {
@@ -13,10 +14,13 @@ class SortAlgorithms extends React.Component {
 				<div className="selectionBar">
 					<Link to="/insertion">Insertion</Link>
 					<Link to="/bubble">Bubble</Link>
+					<Link to="/quicksort">Quicksort</Link>					
 				</div>
 				
   			<Route path="/insertion" render={() => <InsertionSort currentArray={this.props.currentArray}/>} />
   			<Route path="/bubble" render={() => <BubbleSort currentArray={this.props.currentArray}/>} />
+  			<Route path="/quicksort" render={() => <QuickSort currentArray={this.props.currentArray}/>} />
+
   		</div>
 		)
 	}
